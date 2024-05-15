43d680dabfa95124833a10a44943373312525e30
MOHIT <mohit1903sharma@gmail.com>
2024-05-15 23:09:19 +0530

    created basic server

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 #MERN(Project-Amazon)
+
+\*Using Link router-dom property instead of a tag. Using Link the page doesn't refresh.
+
+#backend
+//using type: "module" to use import instead of require to import the packages. in package.json
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
+/* eslint-disable react/jsx-key */
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { Link } from "react-router-dom";
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <div>
         <header>
-          <a href="/">Amazon</a>
+          <Link to="/">Amazon</Link>
         </header>
 
-        <main>List Products</main>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
